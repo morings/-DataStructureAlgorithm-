e8ef3ad0e34d969281b548515725d40e12a4bf80
morings <1620201780@qq.com>
2019-12-03 12:05:09 +0800

    电话号码组合

---
+++ 电话号码组合.js
@@ -0,0 +1,24 @@
+function  f(str) {
+  let map = ['',1,'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+  let num = str.split('')
+  let code = [];
+  num.forEach(item=>{
+    if(map[item]){
+      code.push(map[item])
+    }
+  })
+  let comb = (arr)=>{   
+    let tmp = [];
+    for(let i=0;i<arr[0].length;i++){
+      for(let j=0;j<arr[1].length;j++){
+        tmp.push(arr[0][i]+arr[1][j])
+      }
+    };
+    arr.splice(0,2,tmp);
+    if(arr.length>1){
+      comb(arr)
+    }  
+    return arr[0]
+  }
+  return comb(code);
+}
