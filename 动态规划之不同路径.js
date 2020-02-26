edb008d757930024a84fcbc9ef63312691b35fe2
瞿晨星 <18829040776>
2020-02-26 20:10:35 +0800

    代码提交

---
+++ 动态规划之不同路径.js
@@ -0,0 +1,23 @@
+export default (arr, m, n) => {
+  let dp = (m, n) => {
+    // 检查起始或者目标元素是不是1（被占用了），如果起始或者最后那个格就是1，说明怎么都怎么不到那，直接返回0
+    if (arr[m - 1][n - 1] === 1 || arr[0][0] === 1) {
+      return 0
+    }
+    if (m < 2 || n < 2) {
+      if (m < 2) {
+        return arr[m - 1].includes(1) ? 0 : 1
+      } else {
+        for (let i = 0; i < m; i++) {
+          if (arr[i][0] === 1) {
+            return 0
+          }
+        }
+        return 1
+      }
+    } else {
+      return dp(m - 1, n) + dp(m, n - 1)
+    }
+  }
+  return dp(m, n)
+}
